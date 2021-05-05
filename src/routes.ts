a4611d98298c64324116b8acf02d3edd05cca315
fej0se <80367187+fej0se@users.noreply.github.com>
2021-05-04 22:17:02 -0300

    changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
 import { EpisodesController } from "./controllers/EpisodesController";
-import { SettingsController } from "./controllers/SettingsController";
 
-const settingsController = new SettingsController();
 const episodesController = new EpisodesController();
 
 export const routes = Router();
 
-routes.post("/settings", settingsController.create);
 routes.post("/episodes", episodesController.create);
