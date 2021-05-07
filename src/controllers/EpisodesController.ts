97819a9f0dc07c42e9756ba090bdf19e651b1980
fej0se <80367187+fej0se@users.noreply.github.com>
2021-05-07 20:45:05 -0300

    Update EpisodesController.ts

--- src/controllers/EpisodesController.ts
+++ src/controllers/EpisodesController.ts
@@ -47,6 +47,6 @@ export class EpisodesController {
 
         const episode = await episodesservice.listByEpisode(id);
 
-        return res.json(episode);
+        return res.json(JSON.stringify(episode));
     }
 }
